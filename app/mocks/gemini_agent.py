c38d435bc9d086bdb4709b266396625daad81199
SengTak <sengtak@hotmail.com>
2025-04-27 21:06:05 +0800

    fix: integrate RAG retrieval tool for enhanced data sourcing in chatbot

--- app/mocks/gemini_agent.py
+++ app/mocks/gemini_agent.py
@@ -116,15 +116,36 @@ datastore_grounding_tool = Tool.from_retrieval(
         )
 googlesearch_tool = Tool.from_google_search_retrieval(grounding.GoogleSearchRetrieval())
 
+rag_retrieval_config = rag.RagRetrievalConfig(
+    top_k=10,  # Optional
+    filter=rag.Filter(vector_distance_threshold=0.5),  # Optional
+)
+rag_corpus = rag.get_corpus("projects/neuralnet-manforce/locations/us-central1/ragCorpora/2305843009213693952")
+rag_retrieval_tool = Tool.from_retrieval(
+    retrieval=rag.Retrieval(
+        source=rag.VertexRagStore(
+            rag_resources=[
+                rag.RagResource(
+                    rag_corpus=rag_corpus.name,  # Currently only 1 corpus is allowed.
+                    # Optional: supply IDs from `rag.list_files()`.
+                    # rag_file_ids=["rag-file-1", "rag-file-2", ...],
+                )
+            ],
+            rag_retrieval_config=rag_retrieval_config,
+        ),
+    )
+)
+
 class Chatbot:
     def __init__(self, history: Optional[List["Content"]] = None, model: Optional[str] = "gemini-1.5-flash-002", use_search=False):
         self.model = GenerativeModel(
             model,
             system_instruction=system_instruction)
         self.chat = self.model.start_chat(history=history)
-        self.grounding_tool = [datastore_grounding_tool]
         self.get_person_data = get_person_data
+        #self.grounding_tool = [datastore_grounding_tool]
         #self.grounding_tool = [googlesearch_tool]
+        self.grounding_tool = [rag_retrieval_tool]
 
     """
     def use_rag_tool(self, user_prompt):
